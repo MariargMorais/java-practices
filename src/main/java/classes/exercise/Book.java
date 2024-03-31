249db32e72cecd5c09bfaebe3273974a27fab809
Maria Morais <maria.morais@liferay.com>
2024-03-31 19:28:55 -0300

    Exercise from Module 03 - after refactoring

--- src/main/java/classes/exercise/Book.java
+++ src/main/java/classes/exercise/Book.java
@@ -1,21 +1,22 @@
 package classes.exercise;
+import java.util.ArrayList;
 import java.util.List;
 
 //The Book.java class you can see is mutable at the moment.
 //Your task is to modify it so that it becomes fully immutable according to the instructions given in the lecture.
 //Good luck!
 
-public class Book {
-    private String title;
-    private String author;
-    private int pageCount;
-    private List<String> genres;
+final class Book {
+    private final String title;
+    private final String author;
+    private final int pageCount;
+    private final List<String> genres;
 
     public Book(String title, String author, int pageCount, List<String> genres) {
         this.title = title;
         this.author = author;
         this.pageCount = pageCount;
-        this.genres = genres;
+        this.genres = new ArrayList<>(genres);
     }
 
     public String getTitle() {
@@ -31,22 +32,6 @@ public class Book {
     }
 
     public List<String> getGenres() {
-        return genres;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setPageCount(int pageCount) {
-        this.pageCount = pageCount;
-    }
-
-    public void setGenres(List<String> genres) {
-        this.genres = genres;
+        return new ArrayList<>(genres);
     }
 }
